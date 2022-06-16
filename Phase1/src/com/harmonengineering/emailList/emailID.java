23e6b1c17d5cec52073d5cd55e7cf2e1680f74b6
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-06-16 09:51:10 -0500

    Practice Project 2 - first full functional

--- Phase1/src/com/harmonengineering/emailList/emailID.java
+++ Phase1/src/com/harmonengineering/emailList/emailID.java
@@ -1,5 +1,8 @@
 package com.harmonengineering.emailList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class emailID
 {
     public static String email_list[] = {
@@ -10,9 +13,37 @@ public class emailID
     } ;
     public static void main( String[] args )
     {
-        System.out.println( "email list contents: " + email_list[0] ) ;
-        System.out.println( "email list contents: " + email_list[1] ) ;
-        System.out.println( "email list contents: " + email_list[2] ) ;
-        System.out.println( "email list contents: " + email_list[3] ) ;
+        displayEmailList() ;
+
+        String selectString = new String() ;
+        Scanner scanner = new Scanner( System.in ) ;
+
+        while ( true )
+        {
+            System.out.print( "Enter email:> " ) ;
+            selectString = scanner.next();
+            if ( selectString.equals("exit")) break ;
+            if ( selectString.equals("list")) displayEmailList() ;
+            else
+            {
+                System.out.println(selectString);
+                if (findEmail(selectString))
+                    System.out.println("email found.");
+                else System.out.println("email not found.");
+            }
+        }
+    }
+    public static void displayEmailList( )
+    {
+        for (int i = 0; i < email_list.length; ++i)
+            System.out.println("email list contents: " + email_list[i]);
+    }
+    public static boolean findEmail( String selection )
+    {
+        ArrayList<String> list = new ArrayList<String>( ) ;
+        for ( int i = 0 ; i < email_list.length ; ++i )
+            list.add( email_list[ i ].toLowerCase() ) ;
+        //System.out.println( "Email list contents: " + list ) ;
+        return list.contains(selection.toLowerCase());
     }
 }
