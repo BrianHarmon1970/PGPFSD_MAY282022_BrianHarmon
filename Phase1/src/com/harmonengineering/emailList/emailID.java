47ba68a78c14f7f1ecd47cd73f8d08f359545838
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-06-16 07:46:58 -0500

    Practice Project 2 - Validation of an email - initial commit

---
+++ Phase1/src/com/harmonengineering/emailList/emailID.java
@@ -0,0 +1,18 @@
+package com.harmonengineering.emailList;
+
+public class emailID
+{
+    public static String email_list[] = {
+               "123@java.com" ,
+                "John@doe.com"  ,
+                "jane@doe.com"  ,
+                "August@real.com"
+    } ;
+    public static void main( String[] args )
+    {
+        System.out.println( "email list contents: " + email_list[0] ) ;
+        System.out.println( "email list contents: " + email_list[1] ) ;
+        System.out.println( "email list contents: " + email_list[2] ) ;
+        System.out.println( "email list contents: " + email_list[3] ) ;
+    }
+}
