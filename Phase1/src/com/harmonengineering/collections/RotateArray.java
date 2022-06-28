7b5be7c879b273baa3b000edeeb006647a7de0e3
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-06-28 06:32:37 -0500

    Assisted Practice: 4.1 Array Rotation

---
+++ Phase1/src/com/harmonengineering/collections/RotateArray.java
@@ -0,0 +1,25 @@
+package com.harmonengineering.collections;
+
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+        if(k > nums.length)
+            k=k%nums.length;
+        int[] result = new int[nums.length];
+        if (k >= 0) System.arraycopy(nums, nums.length - k, result, 0, k);
+        int j=0;
+        for(int i=k; i<nums.length; i++){
+            result[i] = nums[j];
+            j++;
+        }
+        System.arraycopy( result, 0, nums, 0, nums.length );
+    }
+    public static void main(String[] args) {
+        RotateArray r = new RotateArray();
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        r.rotate(arr, 5);
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
+
