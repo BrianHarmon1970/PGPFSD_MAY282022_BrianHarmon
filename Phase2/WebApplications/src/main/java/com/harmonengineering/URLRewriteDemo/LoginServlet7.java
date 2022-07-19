67ff214dd9c6518cb469709e3cb804180ecf68b6
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-07-19 13:36:54 -0500

    Phase2 - Assisted Practice: 1.7 Session Tracking Using URL Rewrite

---
+++ Phase2/WebApplications/src/main/java/com/harmonengineering/URLRewriteDemo/LoginServlet7.java
@@ -0,0 +1,44 @@
+package com.harmonengineering.URLRewriteDemo;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet7")
+public class LoginServlet7 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet7() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        String userId = request.getParameter("userid");
+        response.sendRedirect("dashboard7?userid=" + userId);
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
