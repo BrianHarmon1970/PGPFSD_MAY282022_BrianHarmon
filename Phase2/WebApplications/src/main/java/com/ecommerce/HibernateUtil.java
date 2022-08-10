c602c5651fb43ca3a306ca167eef990c8f4f5542
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-08-10 14:15:24 -0500

    Phase2 - Assisted Practice: 3.5 Collection Mapping in Hibernate

--- Phase2/WebApplications/src/main/java/com/ecommerce/HibernateUtil.java
+++ Phase2/WebApplications/src/main/java/com/ecommerce/HibernateUtil.java
@@ -13,6 +13,26 @@ public class HibernateUtil {
 
     static {
         try {
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
+            Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+            sessionFactory = metaData.getSessionFactoryBuilder().build();
+        } catch (Throwable th) {
+            throw new ExceptionInInitializerError(th);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
+
+class HibernateUtil_EX {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
 
             StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder() ;
             StandardServiceRegistry standardRegistry ;
