8fd120aaa5036e600491ac3389f0554dc1cc02e8
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-08-27 16:22:42 -0500

    (rebase Phase3) beginning Phase3-ending Phase2 branch on 2 and 3. synch. Phase3 	modified:   Phase2/FlightDetail/out/artifacts/FlightDetail/FlightDetail.war 	new file:   Phase2/FlightDetail/src/main/resources/SQL/SCHEMA_Phase2_08262022.sql 	modified:   Phase2/FlightDetail/src/main/resources/SQL/table_flight_detail.sql 	new file:   Phase2/FlightDetail/target/classes/SQL/SCHEMA_Phase2_08262022.sql 	modified:   Phase2/FlightDetail/target/classes/SQL/table_flight_detail.sql 	modified:   Phase2/FlightDetail/web/WEB-INF/config.properties 	new file:   Phase3/.gitignore 	new file:   Phase3/Class-Demo/hello-world-spring-boot/.gitignore 	new file:   Phase3/Class-Demo/hello-world-spring-boot/.mvn/wrapper/maven-wrapper.jar 	new file:   Phase3/Class-Demo/hello-world-spring-boot/.mvn/wrapper/maven-wrapper.properties 	new file:   Phase3/Class-Demo/hello-world-spring-boot/mvnw 	new file:   Phase3/Class-Demo/hello-world-spring-boot/mvnw.cmd 	modified:   Phase3/Class-Demo/hello-world-spring-boot/pom.xml 	new file:   Phase3/Class-Demo/hello-world-spring-boot/src/main/java/com/example/demo/Project1Application.java 	new file:   Phase3/Class-Demo/hello-world-spring-boot/src/main/resources/application.properties 	new file:   Phase3/Class-Demo/hello-world-spring-boot/src/test/java/com/example/demo/Project1ApplicationTests.java 	new file:   Phase3/JdbcTemplate/src/applicationContext.xml 	new file:   Phase3/JdbcTemplate/src/com/javatpoint/Employee.java 	new file:   Phase3/JdbcTemplate/src/com/javatpoint/EmployeeDao.java new file:   Phase3/JdbcTemplate/src/com/javatpoint/Test.java 	modified:   Phase3/Project/SportyShoes/pom.xml 	modified:   Phase3/Project/SportyShoes/src/main/java/com/harmonengineering/SportyShoesApplication.java 	modified:   Phase3/Project/SportyShoes/src/main/resources/application.properties 	new file:   Phase3/Project/SportyShoes/src/main/resources/static/DashboardHeader.jsp 	modified:   Phase3/Project/SportyShoes/src/test/java/com/harmonengineering/SportyShoesApplicationTests.java 	new file:   Phase3/SpringBootJDBCExample-master/bin/.mvn/wrapper/MavenWrapperDownloader.class 	new file:   Phase3/SpringBootJDBCExample-master/bin/.mvn/wrapper/maven-wrapper.jar 	new file:   Phase3/SpringBootJDBCExample-master/bin/.mvn/wrapper/maven-wrapper.properties 	new file:   Phase3/SpringBootJDBCExample-master/bin/mvnw 	new file:   Phase3/SpringBootJDBCExample-master/bin/mvnw.cmd 	new file:   Phase3/SpringBootJDBCExample-master/bin/pom.xml 	new file:   Phase3/SpringBootJDBCExample-master/mvnw 	new file:   Phase3/SpringBootJDBCExample-master/mvnw.cmd 	new file:   Phase3/SpringBootJDBCExample-master/pom.xml 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/SpringBootJdbcExampleApplication.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/model/Student.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/repository/StudentRepository.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/repository/StudentRepositoryImpl.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/service/StudentService.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/java/org/arpit/java2blog/service/StudentServiceImpl.java 	new file:   Phase3/SpringBootJDBCExample-master/src/main/resources/application.properties 	new file:   Phase3/SpringBootJDBCExample-master/src/test/java/com/java2blog/SpringBootJDBCExample/SpringBootJdbcExampleApplicationTests.java

--- Phase3/Project/SportyShoes/src/main/java/com/harmonengineering/SportyShoesApplication.java
+++ Phase3/Project/SportyShoes/src/main/java/com/harmonengineering/SportyShoesApplication.java
@@ -2,6 +2,7 @@ package com.harmonengineering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -12,5 +13,14 @@ public class SportyShoesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SportyShoesApplication.class, args);
 	}
+=======
+
+@SpringBootApplication
+public class SportyShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportyShoesApplication.class, args);
+    }
+>>>>>>> 9aa460f ((rebase Phase3) beginning Phase3-ending Phase2 branch on 2 and 3. synch.)
 
 }
--- Phase3/Project/SportyShoes/src/test/java/com/harmonengineering/SportyShoesApplicationTests.java
+++ Phase3/Project/SportyShoes/src/test/java/com/harmonengineering/SportyShoesApplicationTests.java
@@ -6,8 +6,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SportyShoesApplicationTests {
 
+<<<<<<< HEAD
 	@Test
 	void contextLoads() {
 	}
+=======
+    @Test
+    void contextLoads() {
+    }
+>>>>>>> 9aa460f ((rebase Phase3) beginning Phase3-ending Phase2 branch on 2 and 3. synch.)
 
 }
