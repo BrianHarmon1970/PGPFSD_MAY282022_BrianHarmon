4f532e6780504c7af4a6914d578de0eb652f86b3
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-09-30 21:24:23 -0500

    Phase3 Final Project - SportyShoes - REST API and WebApplication

--- Phase3/Project/SportyShoes/src/main/java/com/harmonengineering/SportyShoesApplication.java
+++ Phase3/Project/SportyShoes/src/main/java/com/harmonengineering/SportyShoesApplication.java
@@ -2,7 +2,6 @@ package com.harmonengineering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -13,14 +12,6 @@ public class SportyShoesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SportyShoesApplication.class, args);
 	}
-=======
 
-@SpringBootApplication
-public class SportyShoesApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SportyShoesApplication.class, args);
-    }
->>>>>>> 9aa460f ((rebase Phase3) beginning Phase3-ending Phase2 branch on 2 and 3. synch.)
 
 }
