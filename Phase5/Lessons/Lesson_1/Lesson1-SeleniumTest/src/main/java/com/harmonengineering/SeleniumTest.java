01f476ee94e0042b7b68b540a2910e7d60914fc4
BrianHarmon1970 <85583892+BrianHarmon1970@users.noreply.github.com>
2022-11-30 21:55:36 -0600

    Phase5 - Assisted Practice: 1.4 Handling Various Web Elements

--- Phase5/Lessons/Lesson_1/Lesson1-SeleniumTest/src/main/java/com/harmonengineering/SeleniumTest.java
+++ Phase5/Lessons/Lesson_1/Lesson1-SeleniumTest/src/main/java/com/harmonengineering/SeleniumTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.openqa.selenium.By.name;
 
 public class SeleniumTest
 {
@@ -42,7 +45,7 @@ public class SeleniumTest
         //●	Finding  Web element using Locator Name
         //        a.	Syntax : name =  Name of the element
         //        b.	Example : driver.findElement(By.name(“name”));
-        System.out.println("Element found: " + driver.findElement(By.name("name")));
+        System.out.println("Element found: " + driver.findElement(name("name")));
 
         System.out.println("Step 1.2.4 Using LinkText as a Locator");
         //●	Finding  Web element using Locator Link Text
@@ -159,12 +162,82 @@ public class SeleniumTest
 //        c.	Tag, Class and Attribute
         //    ●	Syntax: “tag.class[attribute=value]”
         //    ●	Example:
-        driver. findElement(By.cssSelector("input.inputtext[tabindex='1']"));
+        driver.findElement(By.cssSelector("input.inputtext[tabindex='1']"));
 
 //        d.	Inner text
         //●	Syntax: “css = tag.contains(“innertext”)”
         //●	Example: driver.findElement(By.cssSelector(font:contains(“Boston”)));
         //driver.findElement(By.cssSelector("h2:contains("Welcome")));
+
+        WebElement el = driver.findElement(By .id("editbox")) ;
+        System.out.println("editbox text:" + el.getAttribute("value")) ;
+        el.clear();
+        el.sendKeys("Hello!");
+        System.out.println("editbox text:" + el.getAttribute("value")) ;
+
+        System.out.println( "editbox exists?: " + (el != null)) ;
+        System.out.println( "is editbox enabled: " + el.isEnabled()) ;
+
+        el = driver.findElement(By.name("linkname")) ;
+
+        System.out.println( "link exists?: " + (el != null)) ;
+        System.out.println( "is link enabled: " + el.isEnabled()) ;
+        System.out.println( "element name is: " + el.getAttribute("name")) ;
+        //el.click();
+
+        el = driver.findElement(By.cssSelector("button")) ;
+        System.out.println( "button exists?: " + (el != null)) ;
+        System.out.println( "button enabled: " + el.isEnabled()) ;
+        el.click();
+
+        el = driver.findElement(By.cssSelector("#id_checkbox")) ;
+        System.out.println( "checkbox exists?: " + (el != null)) ;
+        System.out.println( "checkbox enabled: " + el.isEnabled()) ;
+        System.out.println( "checkbox selected: " + el.isSelected()) ;
+        el.click();
+        System.out.println( "checkbox selected: " + el.isSelected()) ;
+        el.click();
+        System.out.println( "checkbox selected: " + el.isSelected()) ;
+
+        el = driver.findElement(By.cssSelector("input[type=radio]")) ;
+        System.out.println( "radio enabled: " + el.isEnabled()) ;
+        System.out.println( "radio selected: " + el.isSelected()) ;
+        System.out.println( "radio displayed: " + el.isDisplayed()) ;
+
+        el = driver.findElement(By.cssSelector("select")) ;
+        Select sl  = new Select( el ) ;
+        System.out.println( "selectList exists?: " + (el != null)) ;
+        System.out.println( "selectList enabled: " + el.isEnabled()) ;
+        sl.selectByVisibleText("Blue");
+        sl.selectByIndex(1) ;
+        System.out.println( "selectList count: " + sl.getOptions().size()) ;
+
+        el = driver.findElement(By.cssSelector("table")) ;
+        //No.of Columns
+        //List <WebElement> col = driver.findElements(By.xpath(".//*[@id=\"id_tablediv\"]/table/thead/tr/td"));
+
+        List <WebElement> col = driver.findElements(By.xpath(".//*[@id=\"id_tablediv\"]/table/tbody/tr[1]/td"));
+        System.out.println("No of cols are : " +col.size());
+        //No.of rows
+        List <WebElement> rows = driver.findElements(By.xpath(".//*[@id='id_tablediv']/table/tbody/tr/td[1]"));
+        System.out.println("No of rows are : " + rows.size());
+        WebElement cell = driver.findElement(By.xpath(".//*[@id='id_tablediv']/table/tbody/tr[1]/td[1]")) ;
+        System.out.println("cell value: " + cell.getText()) ;
+        cell = driver.findElement(By.xpath(".//*[@id='id_tablediv']/table/tbody/tr[3]/td[3]")) ;
+        System.out.println("cell value: " + cell.getText()) ;
+
+
+        el = driver.findElement(By.cssSelector("html")) ;
+        driver.switchTo().frame("id_iframe");
+        //driver.switchTo().window( el.getDomProperty("id"));
+
+
+
+
+
+
+
+
 
         driver.close() ;
     }
